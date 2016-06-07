1ae0cbb672b0c49dcf230d71873371b3810c6c74
Alessandro Stein <alessandro@alessandrostein.com>
2016-06-06 23:17:06 -0300

    Fix test with return

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
   def webhook
     if request['hub.verify_token'] == Rails.application.secrets.facebook_messenger['validation_token']
-      request['hub.challenge']
+      render json: request['hub.challenge']
     end
   end
 end
