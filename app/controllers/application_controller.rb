28894c023daecf29197c0c74841b3fb4bd4d921e
Alessandro Stein <alessandro@alessandrostein.com>
2016-06-07 00:00:26 -0300

    Response the message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::API
+
+  URI_SEND_FACEBOOK_MESSAGE = "https://graph.facebook.com/v2.6/me/messages?access_token=#{Rails.application.secrets.facebook_messenger['validation_token']}"
+
   def setup
     if request['hub.verify_token'] == Rails.application.secrets.facebook_messenger['validation_token']
       render json: request['hub.challenge']
@@ -9,13 +12,13 @@ class ApplicationController < ActionController::API
     message = request["entry"][0]["messaging"][0]["message"]["text"]
     user_id = request["entry"][0]["messaging"][0]["recipient"]["id"]
 
-    p message
-    p user_id
-
-    render status: 200
+    # p message
+    # p user_id
+    send_message(user_id, message)
+    # render status: 200
   end
 
   def send_message(user_id, message)
-
+    render RestClient.post(URI_SEND_FACEBOOK_MESSAGE, {recipient: user_id, message: message})
   end
 end
