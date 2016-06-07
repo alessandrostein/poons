79198bb2bab1956dfaaced2091bed02e5cd3f219
Alessandro Stein <alessandro@alessandrostein.com>
2016-06-06 23:28:43 -0300

    Rename routes and received callbacks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::API
-  def webhook
+  def setup
     if request['hub.verify_token'] == Rails.application.secrets.facebook_messenger['validation_token']
       render json: request['hub.challenge']
     end
+  end
+
+  def receive
+    p request
+    render status: 200
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/webhook', to: 'application#webhook'
+  get '/webhook', to: 'application#setup'
+  post '/webhook', to: 'application#receive'
 end
