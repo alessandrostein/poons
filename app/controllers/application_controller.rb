7a522244e0b6c1e68495c8c8f51fc520f883d140
Alessandro Stein <alessandro@alessandrostein.com>
2016-06-06 23:08:04 -0300

    Secret key and return

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::API
   def webhook
-
+    if request['hub.verify_token'] == Rails.application.secrets.facebook_messenger['validation_token']
+      request['hub.challenge']
+    end
   end
 end
