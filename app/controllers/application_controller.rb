cf876beddcf7ddf8738863ab34656e4849a18699
Alessandro Stein <alessandro@alessandrostein.com>
2016-06-11 14:52:44 -0300

    Logs requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,13 +22,14 @@ class ApplicationController < ActionController::API
 
   def send_message(user_id, message)
     begin
-
+      p "Mensagem recebida de #{user_id} com a mensagem #{message}"
       metadata = {
-        recipient: { id: 1069123499833828 },
-        message: { text: "Ops" }
+        recipient: { id: user_id },
+        message: { text: "Resposta: #{message}" }
       }
       # RestClient.post(URI_SEND_FACEBOOK_MESSAGE, {:recipient => {:id => user_id}, :message => message})
-      p RestClient.post(URI_SEND_FACEBOOK_MESSAGE, metadata, content_type: :json, accept: :json)
+      RestClient.post(URI_SEND_FACEBOOK_MESSAGE, metadata, content_type: :json, accept: :json)
+      p "Mensagem respondida para #{user_id} com a mensagem Resposta: #{message}"
     rescue => e
       p "Deu erro: #{e}"
     end
